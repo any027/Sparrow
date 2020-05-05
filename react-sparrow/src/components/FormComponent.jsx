93aba5e4a9662a9ef1dfe28427bd422dcbc36d9d
any027 <anthonyyu8@gmail.com>
2020-05-04 22:52:14 -0700

    added a version that does the bare minimum

--- react-sparrow/src/components/FormComponent.jsx
+++ react-sparrow/src/components/FormComponent.jsx
@@ -4,33 +4,72 @@ import '../style/FormComponent.css';
 class FormComponent extends Component {
     constructor(props){
       super(props);
-      //console.log(this.props)
+      this.state = {
+        data: {},
+        submitted_data: null
+      }
       this.model = this.props.json
+      this.JSONToForm = this.JSONToForm.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleChange = this.handleChange.bind(this);
     }
-    // "tag": "input",
-    // "name": "first_name",
-    // "type": "text",
-    // "human_label": "First Name"
+
 
     JSONToForm() {
       return this.model.map((obj,idx) => {
-        return <input  
-                tag = {obj.tag} 
-                name = {obj.name} 
-                type = {obj.type} 
-                human_label = {obj.human_label} 
-        /> 
+        let label = React.createElement('label', {} , obj.human_label)
+        let input = React.createElement(obj.tag, {name:obj.name, type:obj.type, onChange:this.handleChange} )
+
+        if (obj.conditional){
+          let con_name = obj.conditional.name
+ 
+          let con_value = (this.state.data[con_name])
+          if (obj.conditional.show_if(con_value) === true){
+            return React.createElement('div', {}, label,input)
+          }
+          return null
+        } else {
+        
+          return React.createElement('div', {}, label,input)
+        }
       })
     }
 
+    handleSubmit(e) {
+      e.preventDefault();     
+      this.setState({submittedData: this.state.data})
+    }
+
+    handleChange(e) {
+      e.preventDefault();
+
+        let data_form = {}
+        if (e.target.type === 'date'){
+          let date = new Date(e.target.value)
+          data_form[e.target.name] = date
+        } else {
+          data_form[e.target.name] = e.target.value
+        }
+
+        this.setState({data: data_form})
+    }
 
     render(){
       return(
         <div className="FormComponent">
           <div className="Forms">
-            <form>
+            <form onSubmit={this.handleSubmit}>
               {this.JSONToForm()}
+              <button 
+                className="submitButton"
+                type="submit"
+              >
+                Submit
+              </button>
             </form>
+          </div>
+          <div className="Data">
+            {this.state.submittedData ? JSON.stringify(this.state.submittedData) : ""}
           </div>
         </div>
       )
